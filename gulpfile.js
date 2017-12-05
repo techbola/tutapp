72dc781604369aa784eb815b06f4418237ecc1b8
Bola <bfagbowore@gmail.com>
2017-12-05 14:00:58 +0100

    admin master page setup

--- gulpfile.js
+++ gulpfile.js
@@ -12,5 +12,32 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
-    mix.sass('app.scss');
+    mix.sass('app.scss')
+
+        .styles([
+
+            'blog-post.css',
+            'bootstrap.css',
+            'bootstrap.min.css',
+            'font-awesome.css',
+            'metisMenu.css',
+            'sb-admin-2.css',
+            'styles.css',
+            'timeline.css'
+
+        ], './public/css/libs.css')
+
+
+        .scripts([
+
+            'bootstrap.js',
+            'bootstrap.min.js',
+            'jquery.js',
+            'metisMenu.js',
+            'sb-admin-2.js',
+
+        ], './public/js/libs.js')
+
+
+
 });
